a0b8dcdf5070e145f1028ab23e976d3f627b065d
Wang Lei <wanglei.ok@foxmail.com>
2022-12-19 13:37:51 +0800

    增加win linux条件编译

---
+++ gohksdk/win.go
@@ -0,0 +1,49 @@
+//go:build windows && amd64
+
+package gohksdk
+
+/*
+   #cgo CFLAGS: -I../hksdk/win/include/
+   #cgo LDFLAGS: -L../hksdk/win/lib/ -lHCCore -lhcnetsdk
+
+   #include "hksdk.h"
+*/
+import "C"
+import (
+	"fmt"
+)
+
+func CapturePicture(channel, port int, ip, username, passwd, saveFile string) {
+	ret := C.NET_DVR_Init()
+	if int(ret) != 1 {
+		fmt.Printf("NET_DVR_Init failed error code = %v\n", C.NET_DVR_GetLastError())
+		return
+	}
+	defer C.NET_DVR_Cleanup()
+
+	var deviceInfoTmp C.NET_DVR_DEVICEINFO_V30
+
+	lLoginID := C.NET_DVR_Login_V30(C.CString(ip), C.ushort(port), C.CString(username), C.CString(passwd), (C.LPNET_DVR_DEVICEINFO_V30)(&deviceInfoTmp))
+	if lLoginID == -1 {
+		fmt.Printf("Login to Device failed!\r\n")
+		return
+	}
+
+	//组建jpg结构
+	var JpgPara C.NET_DVR_JPEGPARA
+	JpgPara.wPicSize = 0
+	JpgPara.wPicQuality = 0
+
+	if C.NET_DVR_CaptureJPEGPicture(lLoginID, C.int(channel), (C.LPNET_DVR_JPEGPARA)(&JpgPara), C.CString(saveFile)) == 0 {
+		fmt.Printf("抓图失败，错误代码%v\n", C.NET_DVR_GetLastError())
+		return
+	}
+
+	fmt.Printf("抓图成功!\n")
+
+	if C.NET_DVR_Logout_V30(lLoginID) == 0 {
+		fmt.Printf("Logout failed!\n")
+	}
+	lLoginID = -1
+
+}
