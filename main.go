83161a9fa471d00f4b7c6bb5a2ca3337126a9287
Wang Lei <wanglei.ok@foxmail.com>
2022-12-20 11:54:08 +0800

    downfile

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "democapt01/gohksdk"
+import (
+	"democapt01/gohksdk"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
-	gohksdk.CapturePicture(1, 8000, "192.168.112.102", "admin", "sg123456", "sss.jpg")
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	router.Use(cors())
+	router.GET("/captuerPicture", CapturePicture)
+	router.Run(":10086")
+}
+
+func CapturePicture(c *gin.Context) {
+
+	channel := 1
+	port := 8000
+	if v, ok := c.GetQuery("channel"); ok {
+		channel, _ = strconv.Atoi(v)
+	}
+
+	if v, ok := c.GetQuery("port"); ok {
+		port, _ = strconv.Atoi(v)
+	}
+	ip, _ := c.GetQuery("ip")
+	username, _ := c.GetQuery("username")
+	password, _ := c.GetQuery("password")
+
+	id := xid.New()
+	saveFile := fmt.Sprintf("temp/%v.jpg", id.String())
+	ret := gohksdk.CapturePicture(channel, port, ip, username, password, saveFile)
+	if ret != 0 {
+		c.AbortWithStatus(http.StatusBadGateway)
+		return
+	}
+
+	defer os.Remove(saveFile)
+	c.File(saveFile)
+}
+
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
